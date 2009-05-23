daaf6860e63b25c9a1f93a53ac50dc97d88d89dd
Darcy Laycock <sutto@sutto.net>
2009-05-23 10:46:12 +0800

    Refactor the client stuff to remvoe callbacks, add debugging output

--- lib/gitauth/client.rb
+++ lib/gitauth/client.rb
@@ -25,18 +25,10 @@ module GitAuth
     attr_accessor :user, :command
     
     def initialize(user_name, command)
-      GitAuth.logger.debug "Initializing client with command: '#{command}' and user name '#{user_name}'"
+      GitAuth.logger.debug "Initializing client with command: #{command.inspect} and user name #{user_name.inspect}"
       @callbacks = Hash.new { |h,k| h[k] = [] }
       @user      = GitAuth::Users.get(user_name.to_s.strip)
       @command   = command
-    end
-    
-    def on(command, &blk)
-      @callbacks[command.to_sym] << blk
-    end
-    
-    def execute_callback!(command)
-      @callbacks[command.to_sym].each { |c| c.call(self) }
     end
     
     def exit_with_error(error)
@@ -47,24 +39,30 @@ module GitAuth
     
     def run!
       if @user.nil?
-        execute_callback! :invalid_user
+        exit_with_error "An invalid user / key was used. Please ensure it is setup with GitAuth"
       elsif @command.to_s.strip.empty?
-        execute_callback! :invalid_command
+        if user.shell_accessible?
+          exec(ENV["SHELL"])
+        else
+          exit_with_error "SSH_ORIGINAL_COMMAND is needed, mmmkay?"
+        end
       else
         command   = Command.parse!(@command)
         repo      = Repo.get(extract_repo_name(command))
         if command.bad?
-          execute_callback! :bad_command
+          if user.shell_accessible?
+            exec(@command)
+          else
+            exit_with_error "A Bad Command Has Failed Ye, Thou Shalt Not Continue."
+          end
         elsif repo.nil?
-          execute_callback! :invalid_repository
+          exit_with_error "Ze repository you specified does not exist."
         elsif user.can_execute?(command, repo)
-          # We can go ahead.
           git_shell_argument = "#{command.verb} '#{repo.real_path}'"
-          # And execute that soab.
           GitAuth.logger.info "Running command: #{git_shell_argument} for user: #{@user.name}"
           exec("git-shell", "-c", git_shell_argument)
         else
-          execute_callback! :access_denied
+          exit_with_error "These are not the droids you are looking for"
         end
       end
     rescue Exception => e
@@ -72,7 +70,7 @@ module GitAuth
       e.backtrace.each do |l|
         GitAuth.logger.fatal "  => #{l}"
       end
-      execute_callback! :fatal_error
+      exit_with_error "Holy crap, we've imploded cap'n!"
     end
     
     def self.start!(user, command)
